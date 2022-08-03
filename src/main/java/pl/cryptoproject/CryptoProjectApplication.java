28f3425d29544e4d20b1aefb06183da51f052e50
jacek <jacek@szulc.cc>
2022-08-03 21:20:04 +0200

    Initial commit

---
+++ src/main/java/pl/cryptoproject/CryptoProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.cryptoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoProjectApplication.class, args);
+	}
+
+}
